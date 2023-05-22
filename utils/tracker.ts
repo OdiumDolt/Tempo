3c4c14254ad448b488fb9494cb1aec7a7fc2a7cb
OdiumDolt <rohnheppner@gmail.com>
2023-05-22 15:53:44 -0700

    removed UserTrackers

--- utils/tracker.ts
+++ utils/tracker.ts
@@ -15,17 +15,8 @@ interface Tracker {
     name: string,
 }
 
-class UserTrackers {
-    trackers: Tracker[];
-    user_id: string;
-    email: string;
-    constructor(
-        trackers: Tracker[],
-        user_id: string,
-        email: string
-    ){
-        this.trackers = trackers
-        this.user_id = user_id
-        this.email = email
-    }
+
+export {
+    Tracker, 
+    capture
 }
