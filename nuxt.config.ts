137b7c4e08283c058bd1ba87026a98bee7c181c7
Rohn heppner <rohnheppner@gmail.com>
2023-05-24 09:37:37 -0700

    removed useless comments

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     modules: ['@nuxtjs/supabase'],
-    // components:{
-    //     dirs:[
-    //         "~/components",
-    //         {
-    //             path:'~/components/costom/',
-    //             prefix:"cum"
-    //         }
-    //     ]
-    // }
 })
