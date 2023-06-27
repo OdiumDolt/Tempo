3c1ba1cc5c297bd9b1aef729fe6604c7b9ff5f36
OdiumDolt <rohnheppner@gmail.com>
2023-06-26 17:08:32 -0700

    added dynamic movement for selector

--- components/tools/Input/OptionsSelect.vue
+++ components/tools/Input/OptionsSelect.vue
@@ -16,7 +16,8 @@ export default {
             width: null as null | number,
             positions: [] as number[],
             current_position: 0,
-            x_offset: 0
+            x_offset: 0,
+            selected_option: null as null | number,
         }
 
     },
@@ -26,23 +27,59 @@ export default {
                 'font-size': this.size
             }
         },
+        selector_size(){
+            if (process.client && this.selected_option != null){
+                var width = document.getElementById('options-interable_' + this.selected_option.toString())?.clientWidth
+                if (width != undefined){
+                    return {
+                        width: width + "px"
+                    }
+                }
+                else{
+                    return {
+                        width: 0 + 'px'
+                    }
+                }
+            }
+            else{
+                return {
+                }
+            }
+        },
+        selector_position(){
+            if (process.client && this.selected_option != null){
+                var offset = document.getElementById('options-interable_' + this.selected_option.toString())?.offsetLeft
+                var total_offset = document.getElementById("options-select-container")?.offsetLeft
+                if (offset != undefined && total_offset != undefined){
+                    return {
+                        "left":(offset - total_offset - 5) + 'px'
+                    }
+
+                }
+                else{return{}}
+            }
+            else{return{}}
+        }
+    },
+    methods:{
         
     },
     mounted(){
-    }
+        this.selected_option = 0
+    },
 }
 </script>
 
 
 <template>
-<div>
+<div id="options-select-container">
     <div class="options-container" id="options-container">
-        <span v-for="(value, index) in options" class="options" :id="'options-interable_' + index" :style="font_size">
+        <span v-for="(value, index) in options" class="options" :id="'options-interable_' + index" :style="font_size" @click="selected_option = index">
             {{ value }}
         </span>
     </div>
     <div class="selector-box">
-        <div class="selector" :class="selector_size">
+        <div class="selector" :style="[selector_size, selector_position]">
 
         </div>
     </div>
@@ -68,11 +105,14 @@ export default {
 .options
     color: $grey-8
     font-family: 'Reem Kufi Fun', sans-serif
-    font-s
     // thanks Adam Jenkins http://bit.ly/3oV2l4N
     text-align: center
     z-index: 2
     user-select: none
+    border-radius: 6px
+    padding-right: 5px
+    padding-left: 5px
+    box-sizing: border-box
 
 .selector
     position: absolute
@@ -81,10 +121,11 @@ export default {
     z-index: 1
     background-color: $grey-3
     border-radius: 6px
-    box-sizing: border-box
-    left: calc(100% - 100px)
+    padding-right: 5px
+    padding-left: 5px
     top: 50%
     transform: translateY(-50%)
+    transition: width 0.4s, left 0.4s
     
 .selector-box
     position: relative
